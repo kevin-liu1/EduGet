55430a558714a09fd8b93258f6e97105e6b1cff6
David Jiang <nisja123@gmail.com>
2018-12-30 15:43:45 -0500

    assets folder

--- wiseturn/wiseturn/settings.py
+++ wiseturn/wiseturn/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 
 #Shell Plus
 SHELL_PLUS = "ipython"
