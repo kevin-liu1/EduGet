554bf11ad9ef9fa09ac369bf8901d775c88de850
RYMND <kangrt95@gmail.com>
2019-01-02 03:01:13 -0500

    Added a 404 page not found error

---
+++ website/src/components/404nomatch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Nomatch extends Component {
+  render() {
+    return (
+      <p>404 page not found error, please check your address.</p>
+    );
+  }
+}
+
+export default Nomatch;
--- website/src/routes/AppRouter.js
+++ website/src/routes/AppRouter.js
@@ -7,8 +7,11 @@ import UserAgreement from '../components/UserAgreement';
 import ResetPassword from '../components/ResetPassword';
 import Register from '../components/SignUp';
 import Login from '../components/SignIn';
-import NewPassword from '../components/NewPassword';
-import Thankyou from '../components/Thankyou';
+import NewPassword from '../components/NewPassword'; //change user password
+import Thankyou from '../components/Thankyou'; //thank you for creating a new account
+import Nomatch from '../components/404nomatch'; //404 page not foune
+import Testing from '../components/testing';
+
 
 export const history = createHistory();
 
@@ -24,6 +27,8 @@ export const AppRouter = () => (
         <Route path='/password-reset' component={ResetPassword} exact={true}/>
         <Route path="/new-password" component={NewPassword} exact={true} />
         <Route path="/thank-you" component={Thankyou} exact={true} />
+        <Route path="/testing" component={Testing} exact={true} /> //routing for test page
+        <Route component={Nomatch} exact={true}/>
       </Switch>
     </div>
   </Router>
