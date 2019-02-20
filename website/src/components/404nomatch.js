1acac82109f7f18baa735b0042a9a32699be3ba5
RYMND <kangrt95@gmail.com>
2019-02-20 01:44:03 -0500

    Something i promised a while back
    finished 404 page, and thank-you page

--- website/src/components/404nomatch.js
+++ website/src/components/404nomatch.js
@@ -1,81 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import '../styles/info.css'
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    overflow: 'hidden',
-    padding: `0 ${theme.spacing.unit * 3}px`,
-  },
-  paper: {
-    maxWidth: 400,
-    margin: `${theme.spacing.unit}px auto`,
-    padding: theme.spacing.unit * 2,
-  },
-});
-
-
+import Header from './Header';
+import Footer from './Footer';
 
 class Nomatch extends Component {
 
 
   render() {
-    function AutoGridNoWrap(props) {
-      const { classes } = props;
 
-      const theme = createMuiTheme({
-        palette: {
-          primary: { main: '#4384AB' }, // change color of AppBar
-        }
-      });
-
-      var message = "asdfasdfadfafasdfafsadfsdfafd";
 
     return (
-      <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={16}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Typography noWrap>{message}</Typography>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={16}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs>
-            <Typography noWrap>{message}</Typography>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={16}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs>
-            <Typography>{message}</Typography>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-
-      //<div>
-      //  <Grid>
-      //    <p>404 page not found error, please check your address.  Click <Link to="/"> <u>here</u></Link> to return to home page</p>
-      //  </Grid>
-      //</div>
+      <div id="Page">
+        <Header/>
+          <div id="Content">
+            <Grid container direction="column" justify="center" alignItems="center">
+              <Grid item>
+                <img src={require('../assets/eduget-logo.png')} alt="Eduget Logo"/>
+              </Grid>
+              <Grid item>
+                <h2>404 Page Not Found Error</h2>
+              </Grid>
+              <Grid item>
+                <h2>Please check your address.</h2>
+              </Grid>
+              <Grid item>
+                <h2>Click <Link to="/"><u>here</u></Link> to return to home page</h2>
+              </Grid>
+            </Grid>
+          </div>
+          <Footer/>
+        </div>
     );
   }
 }
--- website/src/components/Thankyou.js
+++ website/src/components/Thankyou.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/info.css'
+import Grid from '@material-ui/core/Grid';
 import Header from './Header';
 import Footer from './Footer';
 
 class Thankyou extends Component {
+
+
   render() {
     return (
-      <div>
-        <p>Your information has been submitted, please stayed tuned for further updates (We will send you an email)</p>
-        <Link to="/create-profile"> click here </Link>
+      <div id="Page">
+          <Header/>
+          <div id="Content">
+              <Grid container direction="column" justify="center" alignItems="center">
+                  <Grid item>
+                      <h2>Your information has been submitted, please log in now.</h2>
+                  </Grid>
+                  <Grid item>
+                      <h2>Click <Link to="/"><u>here</u></Link> to return to home page</h2>
+                  </Grid>
+              </Grid>
+          </div>
+          <Footer/>
       </div>
-    );
+      );
+      }
   }
-}
-
 export default Thankyou;
