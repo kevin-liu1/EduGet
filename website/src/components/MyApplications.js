e2ab43d86c12145fc1d1383cd783ebd3ab5a7ecb
RYMND <kangrt95@gmail.com>
2019-04-03 13:56:55 -0400

    Started on appliations

---
+++ website/src/components/MyApplications.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/info.css'
+import Grid from '@material-ui/core/Grid';
+import Header from './Header';
+import Footer from './Footer';
+import axios from 'axios';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const getApplications = ([
+  {name: "McMaster University", status: "Accepted"},
+  {name: "University of Waterloo", status: "Processing"},
+  {name: "University of Toronto", status: "Declined"}])
+
+/*
+const CustomTableCell = withStyles(theme => ({
+head: {
+  backgroundColor: theme.palette.common.black,
+  color: theme.palette.common.white,
+},
+body: {
+  fontSize: 14,
+},
+}))(TableCell);
+*/
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+
+];
+
+class MyApplications extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      applications: []
+    }
+
+    this.currentapplications = this.currentapplications.bind(this);
+  }
+
+
+
+  //update state on load component
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/institution-admin/applications/',{
+      headers: {'Authorization': 'Token ' + localStorage.getItem('token')}
+    }).then((response) => {
+      console.log(response.data)
+      this.setState({applications: response.data.results});
+      console.log(this.state.applications)
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
+  currentapplications(){
+
+  }
+
+  render() {
+    return (
+      <div id="Page">
+
+              <Grid container direction="column" justify="center" alignItems="center">
+                <Header/>
+                  <Grid item>
+                      <h1>Here is a list of your applications</h1>
+                  </Grid>
+                  <Grid item>
+                      <Table>
+                          <TableHead>
+                              <TableRow>
+
+                                  <TableCell>Applications</TableCell>
+                                  <TableCell align="right">Calories</TableCell>
+                                  <TableCell align="right">Fat (g)</TableCell>
+                                  <TableCell align="right">Carbs (g)</TableCell>
+                                  <TableCell align="right">Protein (g)</TableCell>
+                              </TableRow>
+                          </TableHead>
+                            <TableBody>
+                                {rows.map(row => (
+                                  <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                      {row.name}
+                                    </TableCell>
+                                    <TableCell align="right">{row.calories}</TableCell>
+                                    <TableCell align="right">{row.fat}</TableCell>
+                                    <TableCell align="right">{row.carbs}</TableCell>
+                                    <TableCell align="right">{row.protein}</TableCell>
+                                  </TableRow>
+                                ))}
+                          </TableBody>
+                      </Table>
+                  </Grid>
+              </Grid>
+      </div>
+      );
+      }
+  }
+export default MyApplications;
--- website/src/routes/AppRouter.js
+++ website/src/routes/AppRouter.js
@@ -11,12 +11,13 @@ import Register from '../components/SignUp';
 import Login from '../components/SignIn';
 import NewPassword from '../components/NewPassword'; //change user password
 import Thankyou from '../components/Thankyou'; //thank you for creating a new account
-import Nomatch from '../components/404nomatch'; //404 page not foune
+import Nomatch from '../components/404nomatch'; //404 page not found
 import Testing from '../components/testing';
 import CreateProfile from '../components/CreateProfile';
 import Summary from '../components/Summary';
 import School from '../components/SchoolComponents/School.js';
 import SchoolPage from '../components/SchoolComponents/SchoolPage.js';
+import Applications from '../components/MyApplications.js'
 
 
 export const history = createHistory();
@@ -36,9 +37,9 @@ export const AppRouter = () => (
         <PublicRoute path="/create-profile" component={CreateProfile} exact={true} />
         <PublicRoute path="/404" component={Nomatch} exact={true} />
         <PublicRoute path="/summary" component={Summary} exact={true} />
-        <PublicRoute path="/schools" component={School} exact={true}/> 
+        <PublicRoute path="/schools" component={School} exact={true}/>
         <PublicRoute path="/schools/:uid" component={SchoolPage} exact={true} />
-
+        <PublicRoute path="/applications" component={Applications} exact={true}/> //todo make it private after finish
       </Switch>
     </div>
   </Router>
